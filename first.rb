c4b5fea29c688c6c425ca5e3825837c76f54a0b8
sahilgupta0902 <sahil.gupta@diaspark.com>
2019-10-01 12:22:19 +0530

    changed

---
+++ first.rb
@@ -0,0 +1,27 @@
+  
+# # puts 'hello world';   
+# exm = [4, 4.0, "Jose", ]   
+# # puts exm[-3]
+# # exm = Array("a"..."z")   
+
+# puts 'helllo'
+# # exm = Array.new   
+# # puts exm.insert(2,"amir")
+# # puts exm.shift; 
+# # puts exm.length   
+# # puts "#{exm}"
+
+# =begin
+# days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]   
+# puts days.drop(5)   
+# puts days.drop(6)
+# puts days
+# =end
+
+# HOUR = 60
+# DAY = 24*HOUR
+class dog
+@@x=10
+end
+d=dog.new
+$y =d.x 
